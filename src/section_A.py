94f5bd8e5b67159eab0ff277e5345473350e7c94
Alberto <ap2387@cam.ac.uk>
2023-12-05 12:09:27 +0000

    Section A

---
+++ src/section_A.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+import numpy as np
+from matplotlib import pyplot as plt
+import argparse
+import time
+import os, sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+plt.style.use('mphil.mplstyle')
+
+def load_dataset(filename):
+    
+
+def main():
+    
+    
+    
+    
+if __name__ == "__main__":
+    print("=======================================")
+    print("Initialising section A")
+    print("=======================================")
+    start_time = time.time()
+    main()
+    end_time = time.time()
+    print("=======================================")
+    print("Section A finished. Exiting!")
+    print("Time it took to run the code : {} seconds". format(end_time - start_time))
+    print("=======================================")
